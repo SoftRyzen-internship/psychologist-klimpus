ab14b08a6e455b25ab9a4393d3d09e7931cc89de
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-04-10 13:45:51 +0300

    add span for icon, delete iconRed, add active for icons

--- components/ui/Contacts/Contacts.tsx
+++ components/ui/Contacts/Contacts.tsx
@@ -3,25 +3,24 @@ import React from 'react';
 
 import classNames from 'classnames';
 
+import { ContactsProps } from './type';
+
 import Email from '@/public/icons/mail.svg';
 import Tel from '@/public/icons/phone.svg';
-import EmailRed from '@/public/icons/mailRed.svg';
-import TelRed from '@/public/icons/phoneRed.svg';
 
 import common from '@/data/common.json';
 
-type IContacts = {
-  className?: string;
-  isFooter?: boolean;
-};
-
-export const Contacts = ({ className = '', isFooter }: IContacts) => {
+export const Contacts: React.FC<ContactsProps> = ({
+  className = '',
+  isFooter,
+}) => {
   const { email, tel } = common;
 
   const classLink = classNames(
     className,
-    'transition font-roboto text-mainBlack text-sm md:text-base xl:text-lg leading-[1.35] md:leading-[1.35] xl:leading-[1.35] font-normal inline-flex items-center gap-2',
+    'transition font-roboto  text-sm md:text-base xl:text-lg leading-[1.35] md:leading-[1.35] xl:leading-[1.35] font-normal inline-flex items-center gap-2',
     {
+      'contactsLink text-mainBlack active:text-clickAccent': !isFooter,
       'text-white active:text-accent xl:hover:text-rose xl:focus:text-rose ':
         isFooter,
     },
@@ -44,7 +43,9 @@ export const Contacts = ({ className = '', isFooter }: IContacts) => {
           {isFooter ? (
             <Email className="h-[25px] w-[25px]" />
           ) : (
-            <EmailRed className="h-[25px] w-[25px]" />
+            <span className="text-accent">
+              <Email className="h-[25px] w-[25px]" />
+            </span>
           )}
           <span className="py-[3px] md:py-[2px] xl:py-0">{email}</span>
         </Link>
@@ -59,7 +60,9 @@ export const Contacts = ({ className = '', isFooter }: IContacts) => {
           {isFooter ? (
             <Tel className="h-[25px] w-[25px]" />
           ) : (
-            <TelRed className="h-[25px] w-[25px]" />
+            <span className="text-accent">
+              <Tel className="h-[25px] w-[25px]" />
+            </span>
           )}
           <span className="py-[3px] md:py-[2px] xl:py-0">{tel}</span>
         </Link>
