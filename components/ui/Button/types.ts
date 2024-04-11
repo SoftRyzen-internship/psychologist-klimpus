ec1f883ba5083b1fa89c23345e55e9e01874fde0
SerhiiRud <carmagsales@gmail.com>
2024-04-11 16:08:55 +0300

    +string

--- components/ui/Button/types.ts
+++ components/ui/Button/types.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export interface ButtonProps {
-  children: React.ReactNode;
+  children: React.ReactNode | string;
   type: 'submit' | 'button';
   onClick?: () => void;
   className?: string;
