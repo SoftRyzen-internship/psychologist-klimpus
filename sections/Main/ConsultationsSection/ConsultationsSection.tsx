4a489b33dfe6cb4a004244523585aeef5fbb561f
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-04-15 16:07:14 +0300

    use component SectionTitle, change margin to padding in sections

--- sections/Main/ConsultationsSection/ConsultationsSection.tsx
+++ sections/Main/ConsultationsSection/ConsultationsSection.tsx
@@ -1,4 +1,5 @@
 import { ConsultationsList } from '@/components/ui/ConsultationsList';
+import { SectionTitle } from '@/components/ui/SectionTitle';
 
 import consultations from '@/data/consultations.json';
 
@@ -11,9 +12,9 @@ export const ConsultationsSection = () => {
           {preTitle}
         </p>
         <div className="md:w-[527px] xl:w-[1012px]">
-          <h2 className="sectionTitle mb-6 w-[300px] md:mb-10 md:w-full xl:mb-16">
+          <SectionTitle className="mb-6 w-[300px] md:mb-10 md:w-full xl:mb-16">
             {sectionTitle}
-          </h2>
+          </SectionTitle>
           <p className="mb-5 font-roboto text-base font-medium leading-[1.35] md:mb-9 md:text-lg md:leading-[1.35] xl:mb-10 xl:w-[796px] xl:text-xl xl:leading-[1.35]">
             {sectionText}
           </p>
