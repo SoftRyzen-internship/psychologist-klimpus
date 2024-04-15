beb6f52d6cffcacd8b89b028524dd47a39664e53
Tokio <innka-pinnka@ukr.net>
2024-04-15 16:42:17 +0300

    hero done

--- sections/Hero/Hero.tsx
+++ sections/Hero/Hero.tsx
@@ -10,6 +10,9 @@ export const Hero = () => {
     <section className="section">
       <div className="container">
         <div className="flex flex-col items-start md:flex-row md:items-center md:gap-[34px] xl:gap-[69px]">
+          <h1 className="mb-3 inline font-raleway text-large font-semibold uppercase text-mainBlack md:hidden">
+            {data.hero.title3}
+          </h1>
           <Image
             className="aspect-video h-[300px] w-full rounded-[20px] object-cover object-top md:h-[300px] md:w-[328px] xl:h-[584px] xl:w-[598px] smOnly:mb-4"
             src={data.hero.src}
@@ -18,16 +21,16 @@ export const Hero = () => {
             priority
             alt={data.hero.alt}
           />
-          <div className="flex-1 flex-col md:flex-1">
+          <div className="flex flex-col">
             <div className="relative hidden flex-col md:flex md:w-[346px] xl:w-[495px]">
               <h1 className="font-raleway text-large font-semibold uppercase text-mainBlack md:text-[50px] xl:text-[86px] xl:text-extraLarge mdOnly:mb-[13px]">
                 {data.hero.title1}
               </h1>
-              <h2 className="static font-raleway text-large font-semibold uppercase text-mainBlack md:absolute md:right-[15px] md:top-[69px] md:text-right md:text-[50px] xl:right-[-50px] xl:top-[80px] xl:text-[86px] xl:text-extraLarge">
+              <h2 className="static font-raleway text-large font-semibold uppercase text-mainBlack md:absolute md:right-[15px] md:top-[69px] md:text-right md:text-[50px] xl:right-[-50px] xl:top-[90px] xl:text-[86px] xl:text-extraLarge">
                 {data.hero.title2}
               </h2>
             </div>
-            <ul className="mb-3 ml-[25px] list-disc gap-1 font-roboto text-xs font-normal uppercase leading-[1.5] text-accent md:mb-[70px] md:text-sm xl:mb-[50px] xl:mt-[-18px] xl:text-base">
+            <ul className="mb-3 ml-[25px] list-disc gap-1 font-roboto text-xs font-normal uppercase leading-[1.5] text-accent md:mb-[70px] md:text-sm xl:mb-[50px] xl:mt-[-10px] xl:text-base">
               {data.hero.profession.map((profession, index) => (
                 <li key={index}>{profession}</li>
               ))}
@@ -36,7 +39,7 @@ export const Hero = () => {
               linkType="scroll"
               href={data.url.contacts}
               isAccent={true}
-              className="w-full"
+              className="w-full rounded-[20px]"
             >
               {data.linksText.signUp}
             </SiteLink>
