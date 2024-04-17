f96ec9242afc2d1acea2d118ecb3f8f9723ef18a
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-04-17 11:11:21 +0300

    commented TestComponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { TestComponent } from '@/components/common/TestComponent';
+// import { TestComponent } from '@/components/common/TestComponent';
 import { FeaturesSection } from '@/sections/Main/FeaturesSection';
 import { FormSection } from '@/sections/Main/FormSection';
 import { HeroSection } from '@/sections/Main/HeroSection/HeroSection';
@@ -13,7 +13,7 @@ export default function Home() {
       <AboutSection />
       <FeaturesSection />
       <UniquenessSection />
-      <TestComponent />
+      {/* <TestComponent /> */}
       <FormSection />
     </>
   );
