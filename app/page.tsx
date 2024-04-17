4c4d1d04d153916da036e82142aafd3846b8e9b4
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-04-17 12:09:05 +0300

    melnyk/mainRequestsSection

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { FeaturesSection } from '@/sections/Main/FeaturesSection';
 import { FormSection } from '@/sections/Main/FormSection';
 import { HeroSection } from '@/sections/Main/HeroSection/HeroSection';
 import { UniquenessSection } from '@/sections/Main/UniquenessSection';
-
+import { RequestsSection } from '@/sections/Main/RequestsSection';
 import { AboutSection } from '@/sections/Main/AboutSection';
 
 export default function Home() {
@@ -12,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <FeaturesSection />
+      <RequestsSection />
       <UniquenessSection />
       {/* <TestComponent /> */}
       <FormSection />
