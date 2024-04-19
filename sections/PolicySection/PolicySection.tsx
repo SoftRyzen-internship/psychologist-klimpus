864ab7c467d80ef4a6045396f7df16556d69dc44
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-04-19 16:45:57 +0300

    fix polisy styles

--- sections/PolicySection/PolicySection.tsx
+++ sections/PolicySection/PolicySection.tsx
@@ -13,7 +13,7 @@ export const PolicySection = ({ data }: IPolicy) => {
         <SectionTitle className="mb-6 font-raleway text-3xl uppercase md:mb-10 xl:mb-16 xl:w-[819px] xl:text-5xl">
           {policy.policyTitle}
         </SectionTitle>
-        <div className="mainText prose-li:text-mainText  prose  max-w-none prose-h2:mb-4 prose-h2:mt-0 prose-h2:font-roboto prose-h2:text-base prose-h2:font-semibold prose-p:mb-4 prose-p:mt-0  prose-p:font-roboto prose-p:text-mainBlack first:prose-p:mb-0 prose-a:font-roboto prose-a:text-sm prose-a:text-mainBlack prose-ul:m-0  prose-ul:text-mainBlack prose-li:m-0 prose-li:marker:text-mainBlack md:prose-h2:mb-6 md:prose-h2:text-lg md:prose-p:mb-6 xl:prose-h2:mb-6 xl:prose-h2:text-xl xl:prose-p:mb-6 [&>p:nth-child(11)]:mt-4 md:[&>p:nth-child(11)]:mt-6 [&>p:nth-child(2)]:mt-4 md:[&>p:nth-child(2)]:mt-6 [&>p:nth-child(30)]:mb-0">
+        <div className="mainText prose-li:text-mainText  prose  max-w-none prose-h2:mb-4 prose-h2:mt-0 prose-h2:font-roboto prose-h2:text-base prose-h2:font-semibold prose-p:mb-4 prose-p:mt-0  prose-p:font-roboto prose-p:text-mainBlack first:prose-p:mb-0 prose-a:font-roboto prose-a:text-sm prose-a:text-mainBlack prose-ul:m-0  prose-ul:text-mainBlack prose-li:m-0 prose-li:marker:text-mainBlack md:prose-h2:mb-6 md:prose-h2:text-lg md:prose-p:mb-6 xl:prose-h2:mb-6 xl:prose-h2:text-xl xl:prose-p:mb-6  [&>p:nth-child(10)]:mb-0 [&>p:nth-child(11)]:mb-0 [&>p:nth-child(12)]:mb-0      [&>p:nth-child(13)]:mb-0 [&>p:nth-child(14)]:mb-0  [&>p:nth-child(15)]:mt-4  md:[&>p:nth-child(15)]:mt-6 [&>p:nth-child(2)]:mt-4 md:[&>p:nth-child(2)]:mt-6 [&>p:nth-child(34)]:mb-0 ">
           <StructuredText data={data} />
         </div>
       </div>
