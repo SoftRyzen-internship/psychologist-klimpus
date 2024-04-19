aef15db78d92dfda73a4d10dc584f0833348dbfe
SerhiiRud <carmagsales@gmail.com>
2024-04-19 18:25:29 +0300

    added animation

--- components/ui/EducationCard/EducationCard.tsx
+++ components/ui/EducationCard/EducationCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import { EducationCardProps } from './types';
 
@@ -6,11 +7,15 @@ export const EducationCard: React.FC<EducationCardProps> = ({
   id,
   year,
   name,
+  className,
 }) => {
   return (
     <li
       key={id}
-      className="mainText listItem flex gap-[31px] md:w-[403px] md:gap-5 md:leading-[1.5] xl:w-[598px] xl:pr-12 xl:leading-[1.35]"
+      className={classNames(
+        'mainText listItem flex gap-[31px] md:w-[403px] md:gap-5 md:leading-[1.5] xl:w-[598px] xl:pr-12 xl:leading-[1.35]',
+        className,
+      )}
     >
       <p className="text-accent">{year}</p>
       {name}
