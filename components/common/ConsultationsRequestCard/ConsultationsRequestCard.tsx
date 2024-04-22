25558c847ac3eb667109a7f9c73aedbf3513b403
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-04-22 12:30:58 +0300

    use react-responsive for request card consultations

--- components/common/ConsultationsRequestCard/ConsultationsRequestCard.tsx
+++ components/common/ConsultationsRequestCard/ConsultationsRequestCard.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 
+import { useMediaQuery } from 'react-responsive';
 import classNames from 'classnames';
 
 import { ConsultationsRequestCardProps } from './type';
@@ -10,6 +13,10 @@ export const ConsultationsRequestCard: React.FC<
 > = ({ data, className }) => {
   const { images, requestText, alt } = data;
 
+  const isMobile = useMediaQuery({
+    query: '(max-width: 768px)',
+  });
+
   const styles = classNames(
     className,
     'rounded-[20px] md:w-[225px] xl:w-[392px] relative',
@@ -17,22 +24,27 @@ export const ConsultationsRequestCard: React.FC<
 
   return (
     <div className={styles}>
-      <Image
-        width={448}
-        height={218}
-        src={images.mobImage}
-        alt={alt}
-        priority
-        className="rounded-[20px] md:hidden"
-      />
-      <Image
-        width={392}
-        height={525}
-        src={images.tabdeskImage}
-        alt={alt}
-        priority
-        className="rounded-[20px] md:h-[301px] md:w-[225px] xl:h-[525px] xl:w-[392px] smOnly:hidden"
-      />
+      {isMobile ? (
+        <Image
+          width={428}
+          height={218}
+          src={images.mobImage}
+          alt={alt}
+          priority
+          className="rounded-[20px]"
+          style={{ width: '100%', height: 'auto' }}
+        />
+      ) : (
+        <Image
+          width={392}
+          height={525}
+          src={images.tabdeskImage}
+          alt={alt}
+          priority
+          className="h-[301px] w-[225px] rounded-[20px] xl:h-[525px] xl:w-[392px]"
+        />
+      )}
+
       <p className="absolute left-4 top-4 font-roboto text-xl font-semibold leading-[1.35] text-white md:left-5 md:top-5 md:text-[22px] md:leading-[1.35] xl:left-8 xl:top-8 xl:text-2xl xl:leading-[1.35]">
         {requestText}
       </p>
